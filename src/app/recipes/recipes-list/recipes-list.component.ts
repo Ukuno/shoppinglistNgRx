5d44267d25d7dca158aac2a003dcbb5b3035ce5d
ukuno <yukunos123@gmail.com>
2018-03-13 19:50:45 -0700

    creat output of shopping list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,6 +10,7 @@ export class RecipesListComponent implements OnInit {
 
 recipes : Recipe[] = [
 
+	new Recipe('this is the name', 'this is the description', 'https:/\/cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
 	new Recipe('this is the name', 'this is the description', 'https:/\/cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
 	
  ];
