864abdd4f6d8336c3747d7c7c29322d6af0d4286
ukuno <yukunos123@gmail.com>
2018-03-19 13:58:24 -0700

    add navigation between recipe and shopping-list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,8 +10,8 @@ export class RecipesListComponent implements OnInit {
 
 recipes : Recipe[] = [
 
-	new Recipe('this is the name', 'this is the description', 'https:/\/cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-	new Recipe('this is the name', 'this is the description', 'https:/\/cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+	new Recipe('Italian Salad', 'bring all the vegies on the garden, we will make a masterpiece', 'https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=bdc9a3b36bf4095dbcb732b9fab087a3'),
+	new Recipe('Da Bologna Pizzeria', 'the delicious pizza de bologna', 'https://static-news.moneycontrol.com/static-mcnews/2017/07/Pizza_food_FMCG_price_hotel_piece1-770x433.jpg')
 	
  ];
 
