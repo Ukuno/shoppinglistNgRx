91ba395c7f631792be0d9da56d272e65ff75584b
ukuno <yukunos123@gmail.com>
2018-03-19 12:53:22 -0700

    add nav selector on header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+@Output() navSelection = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(navSelect : string){
+
+  	this.navSelection.emit(navSelect);
+
   }
 
 }
