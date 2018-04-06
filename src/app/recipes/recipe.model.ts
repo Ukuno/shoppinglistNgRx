51ce4bbe7bb85254b940a2ac25e3e279d2e8fa17
ukuno <yukunos123@gmail.com>
2018-04-05 23:35:55 -0700

    add ingredient to recipes and display them

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,14 +1,18 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
-	
-	public name : string;
-	public description : string;
-	public imagePath : string;
 
-	constructor( name: string, desc: string, imagePath: string){
+public name: string;
+public description: string;
+public imagePath: string;
+public ingredients: Ingredient[];
 
-		this.name = name;
-		this.description = desc;
-		this.imagePath = imagePath;
+constructor( name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
 
-	}
+      this.name = name;
+      this.description = desc;
+      this.imagePath = imagePath;
+      this.ingredients = ingredients;
+
+   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,32 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-         // tslint:disable-next-line:max-line-length
-        new Recipe('Italian Salad', 'bring all the vegies on the garden, we will make a masterpiece', 'https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=bdc9a3b36bf4095dbcb732b9fab087a3'),
-        // tslint:disable-next-line:max-line-length
-        new Recipe('Da Bologna Pizzeria', 'the delicious pizza de bologna', 'https://static-news.moneycontrol.com/static-mcnews/2017/07/Pizza_food_FMCG_price_hotel_piece1-770x433.jpg')
+
+    new Recipe(
+            'Italian Salad',
+            'bring all the vegies on the garden, we will make a masterpiece',
+            // tslint:disable-next-line:max-line-length
+            'https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=bdc9a3b36bf4095dbcb732b9fab087a3',
+        [
+            new Ingredient('cheese', 5),
+            new Ingredient('tomatoes', 4),
+            new Ingredient('lettuce', 2),
+            new Ingredient('potatoes', 2)
+        ]),
+     new Recipe(
+        'Da Bologna Pizzeria',
+        'the delicious pizza de bologna',
+        'https://static-news.moneycontrol.com/static-mcnews/2017/07/Pizza_food_FMCG_price_hotel_piece1-770x433.jpg',
+    [
+        new Ingredient('tomatoes', 10),
+        new Ingredient('cheese', 5),
+        new Ingredient('Mushrooms', 2)
+    ])
      ];
     getRecipe() {
         return this.recipes.slice();
