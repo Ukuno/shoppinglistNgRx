14fc6d73ba64f9eeaf0bce0dab16be6eebfc735c
ukuno <yukunos123@gmail.com>
2018-05-08 21:37:12 -0700

    make the recipe button clickable

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+
 
 
 @Component({
@@ -13,9 +17,12 @@ export class RecipesListComponent implements OnInit {
 
 recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipe();
+  }
+  newRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
