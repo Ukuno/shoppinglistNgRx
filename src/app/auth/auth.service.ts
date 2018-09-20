3093b25a09eaccfaa1853f3937604bd57915e3d7
ukuno <yukunos123@gmail.com>
2018-09-20 12:22:59 -0700

    add router to the login and logout page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
 
   token: string;
+
+  constructor(private route: Router) {}
 
   signUpUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -15,6 +20,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(
       reponse => {
+        this.route.navigate(['/']);
         firebase.auth().currentUser.getIdToken()
         .then(
           (token: string) => this.token = token
@@ -36,6 +42,12 @@ export class AuthService {
 
   isAuthernticated() {
    return this.token != null;
+  }
+
+  isLogOut() {
+    firebase.auth().signOut();
+    this.token = null;
+    this.route.navigate(['/signin']);
   }
 
 }
