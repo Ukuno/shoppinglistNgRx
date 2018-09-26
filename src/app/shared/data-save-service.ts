fe153b9cc147f4db8fff2697b6065519c78d39ca
ukuno <yukunos123@gmail.com>
2018-09-25 22:22:05 -0700

    add httpclient

--- src/app/shared/data-save-service.ts
+++ src/app/shared/data-save-service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
-import { Http, Response } from '@angular/http';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class DataSave {
-  constructor( private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor( private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+
 
   saveData() {
-    return this.http.put('https://recipe-book-ukuno.firebaseio.com/recipe.json', this.recipeService.getRecipe());
+    const token = this.authService.getToken();
+    return this.httpClient.put('https://recipe-book-ukuno.firebaseio.com/recipe.json', this.recipeService.getRecipe(), {
+      params: new HttpParams().set('auth', token)
+    });
   }
   fetchData() {
     const token = this.authService.getToken();
-    return this.http.get('https://recipe-book-ukuno.firebaseio.com/recipe.json?auth=' + token)
+    return this.httpClient.get<Recipe[]>('https://recipe-book-ukuno.firebaseio.com/recipe.json', {
+      params: new HttpParams().set('auth', token)
+    })
     .pipe(map(
-      (response: Response) => {
-        const recipes: Recipe[] = response.json();
-        for (let recipe of recipes) {
+      (recipes) => {
+        // const recipes: Recipe[] = response.json();
+        for (const recipe of recipes) {
           if (!recipe['ingredients']) {
             recipe['ingredients'] = [];
           }
